317775f95be727182d1abf258211aa52e3be4f4e
Shahzaib Umaar <shahzaib.umaar@technisia.com>
2022-09-23 17:48:15 +0500

    updated server

--- stripe.js
+++ stripe.js
@@ -1,5 +1,3 @@
 import Stripe from "stripe";
 
-export const stripe = Stripe(
-  "sk_test_51Lg5JLB5baEfFW4zjbrAbvu83fQAcJwtJXYHgF2gMi5YoM7XtUZHKGhLEL56Q4hI8NstgDrwlPJzGgcKGr2qOSZB002Y6Pjqxj"
-);
+export const stripe = Stripe(process.env.stripe_key);
